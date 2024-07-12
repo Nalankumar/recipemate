e1d49b4a503d95d7993b040e4ad810801d6375b8
Nalankumar <Nalankumar@users.noreply.github.com>
2024-07-12 17:57:31 +0530

    get request for by id and random recipes

--- src/datafetcher.js
+++ src/datafetcher.js
@@ -1,8 +1,8 @@
 import {data} from './data/data.js'
 import axios from 'axios'
 const apiKey= data.apiKey;
-const url = 'https://api.spoonacular.com/recipes/716429/information?apiKey='+apiKey+'&includeNutrition=true'
-function getData(){
+const url = 'https://api.spoonacular.com/recipes/random?apiKey='+apiKey;
+export function getData(){
     axios.get(url)
     .then(function (response){
         console.log(response);
@@ -12,4 +12,28 @@ function getData(){
     })
     
 }
-getData();
+
+export function getRandomRecipes(id){
+    var urlForRandomRecipes = 'https://api.spoonacular.com/recipes/random?apiKey='+apiKey;
+    var r;
+    axios.get(urlForRandomRecipes)
+    .then(function (res){
+        r =  res.data;
+    })
+    .catch(function (error){
+        alert("Data not found\n" + error);
+    })
+    return r;
+}
+
+export function getInfoById(id){
+    var url = "https://api.spoonacular.com/recipes/"+id+"/information?apiKey=" + apiKey;
+    axios.get(url)
+    .then(function (res){
+        console.log(res);
+    })
+    .catch(function (error){
+        console.log(error);
+    })
+
+}
