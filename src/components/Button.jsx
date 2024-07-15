23b43d90d48bd84e03fbb581ca724b9326039af9
Nalankumar <Nalankumar@users.noreply.github.com>
2024-07-15 15:03:42 +0530

    external commetnts removed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,6 @@ export default function Button({content}){
     return (
         <div>
             <button className="btn btn-primary">Primary</button>
-            {/* <button className="bg-button border-solid border-black-900 w-fit rounded-lg p-[10px] text-[12px]">{content}</button> */}
         </div>
     )
 }
--- src/components/Homepage/Secondarypage/Image.jsx
+++ src/components/Homepage/Secondarypage/Image.jsx
@@ -1,14 +1,9 @@
 
 export default function Image({source}){
     return(
-        // <div className='aspect-auto w-[80vw] rounded-md border-2 shadow-md md:w-[30vw]'>
-        //     <img src={source} alt='image' className='object-cover rounded-lg ' />
-        // </div>
         <div className="card bg-base-100 rounded-xl w-[80vw] shadow-xl md:w-[30vw]">
             <figure>
-                <img
-                src={source}
-                alt="Shoes" />
+                <img src={source} />
             </figure>
             
         </div>
