cd4b90a7ad2eab6bb72863bf4ff631211ba58713
Nalankumar <Nalankumar@users.noreply.github.com>
2024-07-15 15:10:36 +0530

    responsive nav bar bug partially fixed

--- src/components/Navbar/Menuitems.jsx
+++ src/components/Navbar/Menuitems.jsx
@@ -2,7 +2,7 @@ import Button from '../Button'
 
 export default function Menuitems({isOpen}){
     return(
-        <ul className={isOpen?"menu menu-horizontal bg-base-200 rounded-box w-56 md:flex  md:px-[5vw] md:menu-vertical":"hidden menu bg-base-200 rounded-box w-56 md:px-[5vw] menu-horizontal md:menu-vertical"}>
+        <ul className={isOpen?"menu menu-vertical bg-base-200 rounded-box w-56  md:px-[5vw] md:menu-horizontal":"hidden menu bg-base-200 rounded-box w-56 md:px-[5vw] menu-horizontal md:menu-vertical"}>
                 <li><a>Home</a></li>
                 <li><a>Recipes</a></li>
                 <li><Button content="Log in"></Button></li>
